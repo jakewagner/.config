aef80b12244b0b1759ffda7d284a97b542807848
Jacob Wagner <jacobwagner@Jacobs-MacBook-Air.local>
2023-12-13 00:08:22 -0500

    telescop and autocomplete

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,40 @@
+-- column numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.o.statuscolumn = "%s %l %r "
+
+-- theme
+vim.o.background = "dark" 
+vim.cmd("colorscheme gruvbox")
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
---
+++ test.ts
@@ -0,0 +1,12 @@
+const test = () => {
+  console.log('hellow world')
+}
+
+const someFunctinon = () => {
+  const arr = [1, 2, 3]
+  arr.forEach((num) => {
+    return num
+  })
+}
+
+console.log(test())
